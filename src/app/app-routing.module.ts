7c9cdaf320ebc76e967ed794f1fa2a0fc151808b
Alejandra Aravena <janaravena@gmail.com>
2019-10-02 14:39:01 -0300

    Se actualiza routing para el home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)},
+  { path: 'detalle', loadChildren: './pages/detalle/detalle.module#DetallePageModule' },
 ];
 
 @NgModule({
